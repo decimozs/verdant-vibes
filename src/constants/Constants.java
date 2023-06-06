3b395e7d0f28c0f3e35f63acc29521dae3bc99dc
Decimo <marlonadiguemartin548@gmail.com>
2023-06-06 16:32:02 +0800

    init : constants item

---
+++ src/constants/Constants.java
@@ -0,0 +1,28 @@
+package constants;
+
+public class Constants {
+
+	// Base path
+	private static final String BASEPATH = "/resources/";
+	
+	//ItemImage path
+	private static final String ITEM_IMAGE = "/resources/ItemImage/";
+	
+	// CartIcon path
+	private static final String CART_ICON = "/resources/CartIcon/";
+	
+	// DescriptionImage path
+	private static final String DESCRIPTION_IMAGE= "/resources/DescriptionImage/";
+	
+	// forsythia
+	public String forsythia = "Forsythia";
+	public String forsythiaCategory = "Shrubs";
+	public String forsythiaFamily = "Olives";
+	public String forsythiaPrice = "₱ 900.00";
+	public String forsythiaImage = ITEM_IMAGE + "forsythiaItemImage.png";
+	public String forsythiaDescription = DESCRIPTION_IMAGE + "forsythiaDescription.png";
+	public String forsythiaCart = CART_ICON + "forsythiaCartIcon.png";
+	
+	
+	
+}
