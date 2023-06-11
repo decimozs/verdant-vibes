8506302aea1950c93a765ed65e620e30c17d3829
Decimo <marlonadiguemartin548@gmail.com>
2023-06-12 07:27:02 +0800

    add : add local database for the plant

--- src/backend/Table.java
+++ src/backend/Table.java
@@ -1,40 +1,61 @@
 package backend;
 
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
+import java.text.DecimalFormat;
+import components.Home;
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.ImageIcon;
 
 public class Table extends JPanel {
 
-	private JTable table;
+	public JTable table;
     private DefaultTableModel tableModel;
-    private int total = 0;
+    private Home home;
     
     public DefaultTableModel getTableModel() {
         return tableModel;
     }
     
-    public Table() {
+    public Table(Home home) {
+    	this.home = home;
+    	
         tableModel = new DefaultTableModel(
             new Object[][] {},
-            new String[] { "PlantIcon", "PlantName", "Price", "Quantity", "Price" }
+            new String[] { "PlantIcon", "PlantName", "Item Price", "Quantity", "Total" }
         ) {
             Class[] columnTypes = new Class[] { ImageIcon.class, String.class, Double.class, Integer.class, Integer.class };
             public Class getColumnClass(int columnIndex) {
                 return columnTypes[columnIndex];
             }
         };
-
+        
         table = new JTable(tableModel);
         JScrollPane scrollPane = new JScrollPane(table);
 
         add(scrollPane);
         setVisible(true);
     }
+    
+    public int calculateTotalSum() {
+        int totalSum = 0;
+        int totalColumnIndex = 4;
+
+        for (int i = 0; i < tableModel.getRowCount(); i++) {
+        	Object value = tableModel.getValueAt(i, totalColumnIndex);
+            if (value instanceof Integer) {
+                totalSum += (int) value;
+            } else if (value instanceof Double) {
+                totalSum += ((Double) value).intValue();
+            }
+        }
+        return totalSum;
+    }
+    
+    public String formatSum(double amount) {
+    	DecimalFormat df = new DecimalFormat("#,##0");
+    	return df.format(amount);
+    }
+    
 }
